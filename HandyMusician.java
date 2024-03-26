d27553f56244eb5355a9451c8bebff134ed8bc9d
jayrugman <25958133+JayRugMan@users.noreply.github.com>
2024-03-25 23:27:08 -0600

    - now addresses sharp/flat combos - using ♯ and ♭ - now prints key signatures

--- HandyMusician.java
+++ HandyMusician.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class HandyMusician {
 
+
   private static final String[] ALL_NOTES = new String[] {
-    "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"
+    "A", "A♯/B♭", "B", "C", "C♯/D♭", "D", "D♯/E♭", "E", "F", "F♯/G♭", "G", "G♯/A♭"
   };
+
 
   public static int indexCorrection(int index) {
     // Takes index int and allows the index to "wrap" around the key notes in ALL_NOTES array
@@ -16,6 +19,7 @@ public class HandyMusician {
       return index;
     }
   }
+
 
   public static void determineChords(String key) {
 
@@ -57,13 +61,91 @@ public class HandyMusician {
     System.out.println("");
   }
 
+
   public static void determineKeySignature(String key) {
     // Takes key and provides the key signature
+
+    String theSharp = key;
+    String theFlat = key;
+    String returnSharp = "no ♯ signature";
+    String returnFlat = "no ♭ signature";
+
+    String[] sharpSigs = new String[] {
+      "C", "G", "D", "A", "E", "B", "F♯", "C♯"
+    };
+    String[] flatSigs = new String[] {
+      "C", "F", "B♭", "E♭", "A♭", "D♭", "G♭", "C♭"
+    };
+
+    if(key.length() > 1) {
+      theSharp = "" + key.charAt(0) + key.charAt(1);
+      theFlat = "" + key.charAt(3) + key.charAt(4);
+    }
+
+    if(Arrays.asList(sharpSigs).contains(theSharp)) {
+      returnSharp = Arrays.asList(sharpSigs).indexOf(theSharp) + " ♯'s";
+    }
+
+    if(Arrays.asList(flatSigs).contains(theFlat)) {
+      returnFlat = Arrays.asList(flatSigs).indexOf(theFlat) + " ♭'s";
+    }
+
+    System.out.println("\nKey Signatures:");
+    System.out.println(returnSharp + "\n" + returnFlat);
+
   }
+
 
   public static void notesOnFretBoard(String key) {
     // Takes the key and shows which notes on the fretboard are included
   }
+
+
+  public static String sharpFlatCombine(String sharpFlat) {
+    // Takes string of sharp or flat and returns the sharp/flat combo
+    
+    int sfIndex = 0;
+    String[] sharps = new String[] {
+      "A♯", "C♯", "D♯", "F♯", "G♯"
+    };
+    String[] flats = new String[] {
+      "B♭", "D♭", "E♭", "G♭", "A♭"
+    };
+    String[] combos = new String[] {
+      "A♯/B♭", "C♯/D♭", "D♯/E♭", "F♯/G♭", "G♯/A♭"
+    };
+
+    if(Arrays.asList(sharps).contains(sharpFlat)) {
+      sfIndex = Arrays.asList(sharps).indexOf(sharpFlat);
+    } else if(Arrays.asList(flats).contains(sharpFlat)) {
+      sfIndex = Arrays.asList(flats).indexOf(sharpFlat);
+    } else {
+      return sharpFlat;
+    }
+
+    return combos[sfIndex];
+  }
+
+
+  public static String sharpFlatTranslate(String inputStr) {
+    // Converts # to ♯ and b to ♭ 
+
+    String translatedString = "";
+
+    if(inputStr.length() > 1) {
+      if(inputStr.charAt(1) == '#') {
+        translatedString = inputStr.charAt(0) + "♯";
+      } else if(inputStr.charAt(1) == 'B') {
+        translatedString = inputStr.charAt(0) + "♭";
+      } 
+      translatedString = sharpFlatCombine(translatedString);
+    } else {
+      translatedString = inputStr;
+    }
+
+    return translatedString;
+  }
+
 
   public static String getKeyRoot() {
     Scanner scanner = new Scanner(System.in);
@@ -72,6 +154,7 @@ public class HandyMusician {
     while(true) {
       System.out.print("Enter the musical key: ");
       key = scanner.nextLine().toUpperCase();
+      key = sharpFlatTranslate(key);
       if(Arrays.asList(ALL_NOTES).contains(key)) {
         scanner.close();
         return key;
@@ -81,11 +164,12 @@ public class HandyMusician {
     }
   }
 
+
   public static void main(String[] args) {
     String musicalKey = getKeyRoot();
 
     determineChords(musicalKey);
-    //determineKeySignature(musicalKey);
+    determineKeySignature(musicalKey);
     //notesOnFretBoard(musicalKey);
   }
 
