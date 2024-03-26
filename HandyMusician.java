6a6a1abba6e1193d5a603ab96ceb52b636b6816f
jayrugman <25958133+JayRugMan@users.noreply.github.com>
2024-03-26 15:14:04 -0600

    Still broken, but added option to start with key sig or root note

--- HandyMusician.java
+++ HandyMusician.java
@@ -5,8 +5,17 @@ import java.util.Arrays;
 public class HandyMusician {
 
 
+  // List of all 12 keys
   private static final String[] ALL_NOTES = new String[] {
     "A", "A♯/B♭", "B", "C", "C♯/D♭", "D", "D♯/E♭", "E", "F", "F♯/G♭", "G", "G♯/A♭"
+  };
+
+  // Lists of sharp and flag signatures with index representing number of each
+  private static final String[] SHARP_SIGS = new String[] {
+    "C", "G", "D", "A", "E", "B", "F♯", "C♯"
+  };
+  private static final String[] FLAT_SIGS = new String[] {
+    "C", "F", "B♭", "E♭", "A♭", "D♭", "G♭", "C♭"
   };
 
 
@@ -65,31 +74,27 @@ public class HandyMusician {
   public static void determineKeySignature(String key) {
     // Takes key and provides the key signature
 
+    // both theSharp and theFlat initialized with key because "C" is in both lists
     String theSharp = key;
     String theFlat = key;
     String returnSharp = "no ♯ signature";
     String returnFlat = "no ♭ signature";
 
-    String[] sharpSigs = new String[] {
-      "C", "G", "D", "A", "E", "B", "F♯", "C♯"
-    };
-    String[] flatSigs = new String[] {
-      "C", "F", "B♭", "E♭", "A♭", "D♭", "G♭", "C♭"
-    };
-
+    // Turns sharp/flat pairs into single sharp and single flat
     if(key.length() > 1) {
       theSharp = "" + key.charAt(0) + key.charAt(1);
       theFlat = "" + key.charAt(3) + key.charAt(4);
     }
 
-    if(Arrays.asList(sharpSigs).contains(theSharp)) {
-      returnSharp = Arrays.asList(sharpSigs).indexOf(theSharp) + " ♯'s";
+    // Completes final string for sharp and flats if any sig for each
+    if(Arrays.asList(SHARP_SIGS).contains(theSharp)) {
+      returnSharp = Arrays.asList(SHARP_SIGS).indexOf(theSharp) + " ♯'s";
+    }
+    if(Arrays.asList(FLAT_SIGS).contains(theFlat)) {
+      returnFlat = Arrays.asList(FLAT_SIGS).indexOf(theFlat) + " ♭'s";
     }
 
-    if(Arrays.asList(flatSigs).contains(theFlat)) {
-      returnFlat = Arrays.asList(flatSigs).indexOf(theFlat) + " ♭'s";
-    }
-
+    // Final printout
     System.out.println("\nKey Signatures:");
     System.out.println(returnSharp + "\n" + returnFlat);
 
@@ -165,9 +170,87 @@ public class HandyMusician {
   }
 
 
-  public static void main(String[] args) {
-    String musicalKey = getKeyRoot();
+  public static String getRootFromSig() {
+    // Takes signature (number of sharps or flats) and returns a key
 
+    Scanner scanner = new Scanner(System.in);
+    String menu = "Enter number of sharps (n#) or number of flats (nb): ";
+    String sig;
+    char sharpFlat;
+    String key;
+    
+    while(true) {
+      System.out.print(menu);
+      sig = scanner.nextLine().toUpperCase();
+      sig = sharpFlatTranslate(sig);
+      sharpFlat = sig.charAt(1);
+      if(sig.length() == 2) {  // Make sure input is only 2 characters
+        try {
+          int num = Integer.parseInt(sig.charAt(0) + "");  // Make sure first character is an integer
+          if(num >= 0 && num <= 7) {  // Make sure initial integer is 0-7
+            if(sharpFlat == '♯') {
+              key = SHARP_SIGS[num];
+              scanner.close();
+              return key;
+            } else if(sharpFlat == '♭') {
+              key = FLAT_SIGS[num];
+              scanner.close();
+              return key;
+            } else {  // If neither ♯ or ♭
+              System.out.println("Didn't recognize \"" + sharpFlat + "\"");
+            }
+          } else {
+            System.out.println("Initial Integer should be between 0 and 7");
+          }
+        } catch(NumberFormatException e) {
+          System.out.println("The first character should be an integer");
+        }
+      } else {
+        System.out.println("Improper input");
+      }
+    }
+  }
+
+
+  public static int getOption() {
+    // Presents an initial menu and takes an option to determine whether user has root note or signature
+    Scanner scanner = new Scanner(System.in);
+    int opt;
+    String menu = "Do you have 1) the root note; or 2) the key signature (number of #'s or b's): ";
+
+    while(true) {
+      System.out.print(menu);
+      String input = scanner.nextLine();
+
+      try {
+        opt = Integer.parseInt(input);
+        if(opt == 1 || opt == 2) {
+          scanner.close();
+          return opt;
+        } else {
+          System.out.println("Valid options are 1 or 2");
+        }
+      } catch (NumberFormatException e) {
+        System.out.println("Valid options are 1 or 2");
+      }
+
+    } 
+  }
+
+
+  public static void main(String[] args) {
+    
+    String musicalKey = "C";
+    int option = getOption();
+    switch(option) {
+      case 1:
+        musicalKey = getKeyRoot();
+        break;
+      case 2:
+        musicalKey = getRootFromSig();
+        break;
+    }
+    
     determineChords(musicalKey);
     determineKeySignature(musicalKey);
     //notesOnFretBoard(musicalKey);
